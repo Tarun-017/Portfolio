c127e874f88390f7f48c9337980399c3ccd42a09
Tarun Kumar <tarun.kumar098@outlook.com>
2024-12-12 18:34:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,16 @@ document.getElementById('contact-form').addEventListener('submit', function (e) 
         alert("Please fill out all fields.");
     }
 });
+
+// Get the navbar element
+const navbar = document.getElementById('navbar');
+
+// When the user scrolls, execute the function
+window.onscroll = function() {
+  // Check if the page has been scrolled more than 50px
+  if (window.scrollY > 50) {
+    navbar.classList.add('sticky'); // Add class to change background color
+  } else {
+    navbar.classList.remove('sticky'); // Remove class to keep it transparent
+  }
+};
